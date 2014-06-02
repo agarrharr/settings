d66ad80d29a2e09c4aa003def7832a3f43349a54
Adam Harris <aharris88@users.noreply.github.com>
2014-06-02 12:17:03 -0600

    Also output current song to a file

--- config/pianobar/songNotifications.rb
+++ config/pianobar/songNotifications.rb
@@ -2,6 +2,10 @@
 
 require 'cgi'
 
+File.open('pianobar/nowplaying', 'w') do |f2|
+  f2.puts "hello"
+end
+
 trigger = ARGV.shift
 
 if trigger == 'songstart'
@@ -9,4 +13,8 @@ if trigger == 'songstart'
   STDIN.each_line { |line| songinfo.store(*line.chomp.split('=', 2))}
 
   `terminal-notifier -message "Artist: #{songinfo['artist']}\nAlbum: #{songinfo['album']}" -title "#{songinfo['title']}"`
+
+  File.open('pianobar/nowplaying', 'w') do |f2|
+    f2.puts "#{songinfo['title']}\nby #{songinfo['artist']}"
+  end
 end
