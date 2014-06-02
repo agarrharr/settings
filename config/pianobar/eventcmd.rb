d2a5d668df959e7ac874ae26ecd919abb2cf4629
Adam Harris <aharris88@users.noreply.github.com>
2014-06-02 13:25:13 -0600

    Update path to absolute instead of relative in eventcmd.rb

--- config/pianobar/eventcmd.rb
+++ config/pianobar/eventcmd.rb
@@ -8,7 +8,7 @@ if trigger == 'songstart'
   songinfo = {}
   STDIN.each_line { |line| songinfo.store(*line.chomp.split('=', 2))}
 
-  File.open('pianobar/nowplaying', 'w') do |f2|
+  File.open('/Users/aharris88/.config/pianobar/nowplaying', 'w') do |f2|
     f2.puts "#{songinfo['artist']}\n#{songinfo['album']}\n#{songinfo['title']}"
   end
 end
