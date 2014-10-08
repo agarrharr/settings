5742d2cc2f8270e59354087729e3ea06be01e814
Adam Harris <aharris88@users.noreply.github.com>
2014-10-08 10:39:02 -0600

    Make the setup script install oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -32,9 +32,9 @@ install_zsh () {
 # Test to see if zshell is installed.  If it is:
 if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
     # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-    #if [[ ! -d $dir/oh-my-zsh/ ]]; then
-    #    git clone http://github.com/michaeljsmalley/oh-my-zsh.git
-    #fi
+    if [[ ! -d $dir/oh-my-zsh/ ]]; then
+        git clone http://github.com/michaeljsmalley/oh-my-zsh.git
+    fi
     # Set the default shell to zsh if it isn't currently set to zsh
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
         chsh -s $(which zsh)
