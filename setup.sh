74b96d33de44678ec0171530795dfa21aea43808
Adam Harris <aharris88@users.noreply.github.com>
2014-12-08 11:56:38 -0700

    Add tmux plugin manager and tmux-resurrect

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 # list of files/folders to symlink in homedir
-files="config editorconfig gitconfig gitignore oh-my-zsh mutt muttrc tmux.conf ttytterrc vim vimrc zshrc"
+files="config editorconfig gitconfig gitignore oh-my-zsh mutt muttrc tmux tmux.conf ttytterrc vim vimrc zshrc"
 
 ##########
 
