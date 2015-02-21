e2ae0071e82925b28d15cd79bbee980ed56d23e9
Adam Harris <aharris88@users.noreply.github.com>
2015-02-21 09:29:26 -0700

    Don't save pianobar to firebase anymore

--- config/pianobar/eventcmd.rb
+++ config/pianobar/eventcmd.rb
@@ -11,6 +11,4 @@ if trigger == 'songstart'
   File.open('/Users/adamharris/.config/pianobar/nowplaying', 'w') do |f2|
     f2.puts "#{songinfo['artist']}\n#{songinfo['album']}\n#{songinfo['title']}"
   end
-
-  system("ruby ~/.config/pianobar/pianobarFirebase.rb")
 end
