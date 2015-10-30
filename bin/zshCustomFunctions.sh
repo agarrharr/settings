8994a4e5a1bdb03724c812f26566c1f83b9b94ec
Adam Harris <aharris88@users.noreply.github.com>
2015-10-30 11:12:12 -0600

    Add base64EncodeSvg to custom zsh functions

--- bin/zshCustomFunctions.sh
+++ bin/zshCustomFunctions.sh
@@ -2,3 +2,9 @@ function getSHA() {
     git log -1 --pretty=format:%H | pbcopy
     echo "Latest SHA1 has been copied to the clipboard"
 }
+
+function base64EncodeSvg() {
+    # pass in path to svg file
+    echo "background: url('data:image/svg+xml;base64,"$(openssl base64 < $1)"');"
+    echo "background: url('data:image/svg+xml;base64,"$(openssl base64 < $1)"');" | pbcopy
+}
