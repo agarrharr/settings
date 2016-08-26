5e8a6b5013d0e839b65bd2cc3f8a9a69c20e9886
Adam Harris <aharris88@users.noreply.github.com>
2016-08-26 14:55:48 -0600

    Fix problem with symlink getting placed inside of directory by deleting it first

--- setup.sh
+++ setup.sh
@@ -24,6 +24,7 @@ echo "done"
 for file in $files; do
     echo "Moving $file from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
+    rm -rf ~/.$file
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
