9b2d3b9142fa243dad7a8cf2efefe382324cc765
Adam Harris <aharris88@users.noreply.github.com>
2016-09-06 11:43:52 -0600

    Add fancy_echo

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,12 @@ set -euo pipefail
 # Ask for the administrator password upfront
 sudo -v
 
+fancy_echo() {
+  echo $'\e[32;1m'$1$'\e[0m'
+}
+
 function install_zsh () {
+  fancy_echo 'Installing zsh'
   # Test to see if zshell is installed.  If it is:
   if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
       # Set the default shell to zsh if it isn't currently set to zsh
@@ -28,29 +33,32 @@ function install_zsh () {
 }
 
 function submodules () {
+  fancy_echo 'Installing submodules'
   cd ~/settings && git submodule init && git submodule update && cd -
 }
 
 function homebrew_packages {
-
   # Install or update Homebrew
-
   which -s brew
   if [[ $? != 0 ]] ; then
+      fancy_echo 'Installing Homebrew'
       # Install Homebrew
       /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
       sudo chown -R $(whoami) /usr/local
   else
+      fancy_echo 'Updating Homebrew'
       brew update
   fi
 
   # Use Homebrew to install command line packages
+  fancy_echo 'Installing Homebrew packages'
   for package in $1; do
     brew install $package
   done
 }
 
 function npm_packages {
+  fancy_echo 'Installing npm packages'
 
   n latest
 
@@ -61,6 +69,8 @@ function npm_packages {
 }
 
 function dotfiles {
+  fancy_echo 'Copying dotfiles'
+
   # Create dotfile symlinks in home directory
   for file in $(find ~/settings/dotfiles -depth 1 -exec basename {} \;); do
     rm -rf ~/$file
@@ -69,6 +79,7 @@ function dotfiles {
 }
 
 function nvim_plugins {
+  fancy_echo 'Installing Neovim plugins'
 
   # required for deoplete plugin in neovim
   pip3 install neovim
@@ -81,6 +92,7 @@ function nvim_plugins {
 }
 
 function gui_apps {
+  fancy_echo 'Installing GUI apps'
 
   # Use Homebrew Cask to install gui apps
   for app in $1; do
@@ -89,6 +101,7 @@ function gui_apps {
 }
 
 function ruby {
+  fancy_echo 'Installing Ruby gems'
   gem install bundler
   gem install timetrap
 }
