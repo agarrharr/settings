9678ef067eb3fcdc756fbfc99d5370d3f263bddb
Adam Harris <aharris88@users.noreply.github.com>
2016-10-03 13:42:40 -0600

    Add alt-is-meta option in hyperterm

--- dotfiles/.hyperterm.js
+++ dotfiles/.hyperterm.js
@@ -3,7 +3,10 @@ module.exports = {
     fontSize: 12,
     fontFamily: 'Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
     padding: '12px 14px',
-    summonShortcut: 'Alt+Space'
+    summonShortcut: 'Alt+Space',
+    modifierKeys: {
+      altIsMeta: true
+    }
   },
   plugins: ['nova-hyperterm', 'hyperterm-summon']
 };
