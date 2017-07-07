40f015620d75889b1178498c11ded33dae4bd5fe
Adam Harris <aharris88@users.noreply.github.com>
2017-07-07 17:34:36 -0600

    Add GNU+Linux settings

---
+++ gnu+linux/readme.md
@@ -0,0 +1,11 @@
+# Settings
+
+This directory includes all of my custom settings and dotfiles for GNU+Linux.
+
+I am currently using Ubuntu 16.04.2 LTS.
+
+The `scripts/install.sh` script will:
+
+1. Install packages
+1. Create symlinks to the dotfiles in `~/settings/dotfiles/` in your home directory and backup old dotfiles to `~/.OLD_DOTFILES`
+
---
+++ gnu+linux/scripts/install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+function green_echo {
+  GREEN='\033[0;32m'
+  NC='\033[0m'
+  echo -e "${GREEN}$1${NC}"
+}
+
+function red_echo {
+  RED='\033[0;31m'
+  NC='\033[0m'
+  echo -e "${RED}$1${NC}"
+}
+
+
+function uninstall_packages {
+  apps='unity-webapps-common'
+
+  for app in $apps; do
+    which $app > /dev/null
+    if [ $? == 0 ]; then
+      green_echo "Uninstalling ${app}"
+      sudo apt-get --assume-yes purge $app
+    fi
+  done
+}
+
+function install_packages {
+  green_echo 'Installing packages'
+  apps=$(cat ~/Development/github/aharris88/settings/gnu+linux/packages.txt)
+
+  for app in $apps; do
+    which $app > /dev/null
+    if [ $? == 1 ]; then
+      green_echo "Installing ${app}"
+      sudo apt-get install --assume-yes $app
+    fi
+  done
+}
+
+function copy_dotfiles {
+  green_echo 'Copying dotfiles'
+
+  mkdir -p ~/.OLD_DOTFILES
+
+  for file in $(find ~/Development/github/aharris88/settings/gnu+linux/dotfiles -maxdepth 1 -mindepth 1 -exec basename {} \;); do
+    if [ -e ~/$file ]
+      then mv ~/$file ~/.OLD_DOTFILES/$file
+    fi
+    ln -s ~/Development/github/aharris88/settings/gnu+linux/dotfiles/$file ~/$file
+  done
+}
+
+function install {
+  uninstall_packages
+  copy_dotfiles
+  install_packages
+}
+
+install
